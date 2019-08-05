20f538f2c973e06d486353e25516f5d11ae07ab6
Drew Foehn <drew@pixelburn.net>
2019-08-05 01:14:47 -0400

    added material-ui

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,21 @@ import App, { Container } from "next/app";
 import React from "react";
 import withApolloClient from "../lib/with-apollo-client";
 import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../lib/theme";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </ThemeProvider>
       </Container>
     );
   }
