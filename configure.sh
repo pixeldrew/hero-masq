8664e983d0b2a866de50e6272fd10c02dd52324b
Drew Foehn <drew@pixelburn.net>
2020-02-27 08:38:03 -0500

    updated docker config

--- configure.sh
+++ configure.sh
@@ -4,37 +4,48 @@ if [ -z "${DNS}" ] ; then
 	DNS="172.31.0.32"
 fi
 
-cat << CONFIG >> /etc/dnsmasq.conf
+cat << CONFIG > /etc/dnsmasq.d/base.conf
+log-facility=-
 server=${DNS}
 domain-needed
-no-resolv # otherwise unknown arpa requests will flood the internal docker dns back and forth
-dhcp-leasefile=/var/lib/dnsmasqd/dnsmasq.leases
 bogus-priv
-domain-needed
+no-hosts
+keep-in-foreground
+no-resolv
+expand-hosts
+dhcp-leasefile=/var/lib/dnsmasqd/dnsmasq.leases
 CONFIG
 
 cat << CONFIG > /etc/supervisord.conf
 [supervisord]
-nodaemon=true
-user=root
+nodaemon = true
+user = root
 
 [supervisorctl]
-serverurl=unix://%(here)s/supervisor.sock
+serverurl = unix:///var/run/supervisor.sock
+username = supervisor
+password = supervisor
+
+[unix_http_server]
+file = /var/run/supervisor.sock
+chmod = 0700
+username = supervisor
+password = supervisor
 
 [rpcinterface:supervisor]
 supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
 
 [program:dnsmasq]
-command=/usr/sbin/dnsmasq -k --log-queries
-stdout_logfile=/var/log/supervisor/%(program_name)s.log
-stderr_logfile=/var/log/supervisor/%(program_name)s_error.log
-autorestart=true
+command = /usr/sbin/dnsmasq -k
+stdout_logfile = /var/log/supervisor/%(program_name)s.log
+stderr_logfile = /var/log/supervisor/%(program_name)s.log
+autorestart = true
 
 [program:hero-masq]
-directory=/usr/src/hero-masq
-command=/usr/bin/npm start
-stdout_logfile=/var/log/supervisor/%(program_name)s.log
-stderr_logfile=/var/log/supervisor/%(program_name)s_error.log
+directory = /usr/src/hero-masq
+command = /usr/bin/npm start
+stdout_logfile = /var/log/supervisor/%(program_name)s.log
+stderr_logfile = /var/log/supervisor/%(program_name)s_error.log
 CONFIG
 
 
